aa26db05c4780b2cecbce6fbeb45b4d4578c3915
xuxiaomeng <xuxiaomeng@baozou.com>
2016-10-27 15:01:15 +0800

    feature. add page2

--- root/src/index/index.jsx
+++ root/src/index/index.jsx
@@ -3,6 +3,7 @@ const React = require('react');
 require('./g.scss')
 
 import Page1 from './page1/page1'
+import Page2 from './page2/page2'
 
 export default class Index extends React.Component{
   constructor(props) {
@@ -12,6 +13,7 @@ export default class Index extends React.Component{
     return (
       <div className="container">
         <Page1 />
+        <Page2 />
       </div>
     )
   }
